a82a8b1b9071063d494713b578d9afb97b6a8e2e
Gianluca Cornacchia <gcornacchia@elettronicagf.it>
2017-05-23 11:41:32 +0200

    removed udev patch

--- scripts/setup-egf.sh
+++ scripts/setup-egf.sh
@@ -1,6 +1,4 @@
 cp scripts/egf-setup-release.sh ../../
-cd ../poky
-git am ../meta-egf/patch/0001-fix-udev-conflict.patch
 
 cd ../meta-fsl-arm
 git am ../meta-egf/patch/0002-fix-sdcard-image-creation-to-allow-dtb-with-.-inside.patch
