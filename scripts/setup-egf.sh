c6cd8409bd8ced7e5536e41d7ea5870f96ac25cd
yocto <yocto@androidbuilder>
2017-07-27 15:43:06 +0200

    added patch to fix yocto bug when image is readonly

--- scripts/setup-egf.sh
+++ scripts/setup-egf.sh
@@ -1,5 +1,7 @@
 cp scripts/egf-setup-release.sh ../../
 
 cd ../meta-fsl-arm
-git am ../meta-egf/patch/0002-fix-sdcard-image-creation-to-allow-dtb-with-.-inside.patch
+git am ../meta-cefla/patch/0002-fix-sdcard-image-creation-to-allow-dtb-with-.-inside.patch
+cd ../meta-fsl-bsp-release
+git am ../meta-cefla/patch/0003-fix-gpuload-offline-install.patch
 
