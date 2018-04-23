2364872ac3f1b5aaa2301f8d33766eea728c7256
yocto <yocto@androidbuilder>
2018-04-23 13:56:00 +0200

    Upgrade to QT 5.8

--- scripts/setup-egf.sh
+++ scripts/setup-egf.sh
@@ -4,4 +4,7 @@ cd ../meta-fsl-arm
 git am ../meta-egf/patch/0002-fix-sdcard-image-creation-to-allow-dtb-with-.-inside.patch
 cd ../meta-fsl-bsp-release
 git am ../meta-egf/patch/0003-fix-gpuload-offline-install.patch
-
+cd ../meta-qt5
+git checkout -b qt5.8_egf ff073f04109900fc07bf81e2f1df63c626caf342
+cd ../meta-fsl-bsp-release
+git am ../meta-egf/patch/0004-remove-fix-eglfs_kms_egldevice-builderror.patch.patch
