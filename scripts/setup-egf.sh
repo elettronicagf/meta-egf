8832c3e3c2e29a7afe87fe58560bbc48c4e3dc00
yocto <yocto@androidbuilder>
2019-03-18 16:26:14 +0100

    updated scripts
    -fix meta-fsl-bsp-release
    -fix local.conf:
     setup path for internal builds
     added rm_work

--- scripts/setup-egf.sh
+++ scripts/setup-egf.sh
@@ -5,3 +5,6 @@ git am ../meta-egf/patch/0001-fix-udev-conflict.patch
 cd ../meta-fsl-arm
 git am ../meta-egf/patch/0002-fix-sdcard-image-creation-to-allow-dtb-with-.-inside.patch
 
+cd ../meta-fsl-bsp-release
+git checkout dfbc90ee74624ce3be636c8bd2a47114fa2b71aa
+
