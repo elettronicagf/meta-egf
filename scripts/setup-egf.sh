ecfc3cf3d25f49414deda4114e7afbf6342f4bb5
yocto <yocto@androidbuilder>
2020-06-09 11:41:45 +0200

    setup-egf: fix git.freescale.com -> source.codeaurora.org

--- scripts/setup-egf.sh
+++ scripts/setup-egf.sh
@@ -8,3 +8,6 @@ git am ../meta-egf/patch/0002-fix-sdcard-image-creation-to-allow-dtb-with-.-insi
 cd ../meta-fsl-bsp-release
 git checkout dfbc90ee74624ce3be636c8bd2a47114fa2b71aa
 
+grep -r git.freescale.com/imx ../* | awk -F: '{print $1}' | xargs -I% sed -i "s/git:\/\/git.freescale.com\/imx/git:\/\/source.codeaurora.org\/external\/imx/g" %
+grep -r linux-2.6-imx.git ../* | awk -F: '{print $1}' | xargs -I% sed -i "s/linux-2.6-imx.git/linux-imx.git/g" %
+
