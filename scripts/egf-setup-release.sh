d87f06ac323dd1728aff2fab07f812eb4ce8eb4e
yocto <yocto@androidbuilder>
2022-03-18 13:53:01 +0000

    Aggiunte variabili d'ambiente YOCTO_BASE_PATH, DL_DIR, SSTATE_DIR, TMPDIR al file scripts/egf-setup-release.sh

--- scripts/egf-setup-release.sh
+++ scripts/egf-setup-release.sh
@@ -208,5 +208,26 @@ if [ -d ../sources/meta-freescale ]; then
 fi
 
 cd  $BUILD_DIR
+
+# *** Modified egf ***********************************************
+if [ $(hostname) = "dockerbuilder" ]; then
+
+YOCTO_BASE_PATH=\"/home/yocto\"
+DL_DIR=\"/home/yocto/downloads\"
+SSTATE_DIR=\"/home/yocto/yocto-output/\${MACHINE}/sstate-cache\"
+TMPDIR=\"/home/yocto/yocto-output/\${MACHINE}/tmp\"
+
+echo YOCTO_BASE_PATH = $YOCTO_BASE_PATH >> ./conf/local.conf
+echo DL_DIR = $DL_DIR >> ./conf/local.conf
+echo SSTATE_DIR = $SSTATE_DIR >> ./conf/local.conf
+echo TMPDIR = $TMPDIR >> ./conf/local.conf
+
+else
+        echo "no dedicated variables for this host"
+fi
+# ****************************************************************
+
+echo "CORE_IMAGE_EXTRA_INSTALL += \"chromium-ozone-wayland\"" >> ./conf/local.conf
+
 clean_up
 unset FSLDISTRO
