ef4053354b1acf8534a203ad1c63d55e0c8ae427
yocto <yocto@elettronicagf.it>
2022-04-26 09:43:50 +0200

    aggiunta patch per mancato download di nfs-utils

--- scripts/setup-egf.sh
+++ scripts/setup-egf.sh
@@ -2,5 +2,7 @@ cp scripts/egf-setup-release.sh ../../
 
 cd ../poky
 git am ../meta-egf/patch/0001-fix-opkg-name-validity-check.patch
+git am ../meta-egf/patch/0004-fix-nfs_utils_xz_to_gz.patch
+
 cd ../meta-fsl-bsp-release
 git am ../meta-egf/patch/0003-fix-missing-imx6ull-platform-in-imx-lib.patch
