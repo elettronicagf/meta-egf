85585fd22d3df5d473d03e36bd79455a9b22c783
yocto <yocto@androidbuilder>
2023-03-01 15:25:37 +0100

    Refactor moduli scripts/ e conf/ ( da testare)

---
+++ scripts/egf-conf/egf-patches-project.sh
@@ -0,0 +1,12 @@
+
+# This map lists patch source and destination : "patch_src patch_dest" 
+# - patch_src -> path of the patch starting from ./meta-egf
+# - patch_dest-> path of the destination from  source (../meta-egf)
+
+PATCH_MAP_PROJECT=(\
+"patch/0001-Only-DRI-for-x11.patch meta-browser/meta-chromium/recipes-browser/chromium/files/" \
+"meta-browser/meta-chromium/recipes-browser/chromium/chromium-gn.inc meta-browser/meta-chromium/recipes-browser/chromium/" \
+"meta-imx/meta-sdk/recipes-fsl/images/imx-image-multimedia.bb meta-imx/meta-sdk/recipes-fsl/images/" \
+ )
+
+
