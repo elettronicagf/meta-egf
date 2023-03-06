0f23fec289df27afa9fa844edcf86942846e0f1c
Simone Peraccini <yocto@dockerbuilder>
2023-03-06 13:10:53 +0000

    Ricette ricollocate in layer -meta-egf-release. Modifica percorsi patch.

--- scripts/egf-conf/egf-patches-project.sh
+++ scripts/egf-conf/egf-patches-project.sh
@@ -5,8 +5,7 @@
 
 PATCH_MAP_PROJECT=(\
 "patch/0001-Only-DRI-for-x11.patch meta-browser/meta-chromium/recipes-browser/chromium/files/" \
-"meta-browser/meta-chromium/recipes-browser/chromium/chromium-gn.inc meta-browser/meta-chromium/recipes-browser/chromium/" \
-"meta-imx/meta-sdk/recipes-fsl/images/imx-image-multimedia.bb meta-imx/meta-sdk/recipes-fsl/images/" \
+"recipes-browser/chromium/chromium-gn.inc meta-browser/meta-chromium/recipes-browser/chromium/" \
  )
 
 
