7b04dd3e2588ec25b99fa95bf1d723c5dab90024
yocto <yocto@androidbuilder>
2023-07-21 16:49:39 +0200

    Patch chromium

--- scripts/egf-conf/egf-patches-project.sh
+++ scripts/egf-conf/egf-patches-project.sh
@@ -4,6 +4,8 @@
 # - patch_dest-> path of the destination from  source (../meta-egf)
 
 PATCH_MAP_PROJECT=(\
+"patch/0013-Only-DRI-for-x11.patch meta-browser/meta-chromium/recipes-browser/chromium/files/" \
+"recipes-browser/chromium/chromium-gn.inc meta-browser/meta-chromium/recipes-browser/chromium/" 
  )
 
 
